b6875c8b16523b3d45607a128dd2faeeac3d9d7b
freeDom- <dominik.weinrich@gmx.de>
2024-11-07 21:27:45 +0100

    Add rotation symmetry

--- src/games/Tafl/XNTupleFuncsTafl.java
+++ src/games/Tafl/XNTupleFuncsTafl.java
@@ -44,7 +44,7 @@ public class XNTupleFuncsTafl
     @Override
     public int getNumSymmetries()
     {
-        return 3;
+        return 6;
     }
 
     @Override
@@ -65,13 +65,13 @@ public class XNTupleFuncsTafl
     @Override
     public BoardVector[] symmetryVectors(BoardVector boardVector, int n)
     {
-        BoardVector[] symmetries = new BoardVector[3];
+        BoardVector[] symmetries = new BoardVector[getNumSymmetries()];
         symmetries[0] = boardVector;
         symmetries[1] = mirrorBoard(boardVector, Axis.VERTICAL);
         symmetries[2] = mirrorBoard(boardVector, Axis.HORIZONTAL);
-        //symmetries[3] = rotate(boardVector);
-        //symmetries[4] = rotate(symmetries[3]);
-        //symmetries[5] = rotate(symmetries[4]);
+        symmetries[3] = rotateBoard(boardVector, Rotation.CLOCKWISE);
+        symmetries[4] = rotateBoard(symmetries[3], Rotation.CLOCKWISE);
+        symmetries[5] = rotateBoard(symmetries[4], Rotation.CLOCKWISE);
         return symmetries;
     }
 
@@ -80,19 +80,43 @@ public class XNTupleFuncsTafl
     {
         Point[] move = TaflUtils.getMoveFromActionNumber(actionKey);
 
+        int[] symmetries = new int[getNumSymmetries()];
+
         // mirror vertically
         // start = 2, 5 -> 4, 5
-        Point verticalMirroredStartPoint = new Point(TaflConfig.BOARD_SIZE - move[0].x - 1, move[0].y);
-        Point verticalMirroredEndPoint = new Point(TaflConfig.BOARD_SIZE - move[1].x - 1, move[1].y);
+        Point verticalMirroredStartPoint = new Point(TaflConfig.BOARD_SIZE - 1 - move[0].x, move[0].y);
+        Point verticalMirroredEndPoint = new Point(TaflConfig.BOARD_SIZE - 1 - move[1].x, move[1].y);
         int verticalMirroredAction = TaflUtils.getActionNumberFromMove(verticalMirroredStartPoint, verticalMirroredEndPoint);
 
         // mirror horizontally
         // start = 2, 5 -> 2, 1
-        Point horizontalMirroredStartPoint = new Point(move[0].x, TaflConfig.BOARD_SIZE - move[0].y - 1);
-        Point horizontalMirroredEndPoint = new Point(move[1].x, TaflConfig.BOARD_SIZE - move[1].y - 1);
+        Point horizontalMirroredStartPoint = new Point(move[0].x, TaflConfig.BOARD_SIZE - 1 - move[0].y);
+        Point horizontalMirroredEndPoint = new Point(move[1].x, TaflConfig.BOARD_SIZE - 1 - move[1].y);
         int horizontalMirroredAction = TaflUtils.getActionNumberFromMove(horizontalMirroredStartPoint, horizontalMirroredEndPoint);
 
-        return new int[] {actionKey, verticalMirroredAction, horizontalMirroredAction};
+        // rotate clockwise
+        // start = 2, 5 -> 1, 2
+        //         3, 5 -> 1, 3
+        //         4, 6 -> 0, 4
+        //         6, 4 -> 2, 6
+        //         1, 2 -> 4, 1
+        Point rotatedStartPoint = new Point(TaflConfig.BOARD_SIZE - 1 - move[0].y, move[0].x);
+        Point rotatedEndPoint = new Point(TaflConfig.BOARD_SIZE - 1 - move[1].y, move[1].x);
+        int rotatedAction = TaflUtils.getActionNumberFromMove(rotatedStartPoint, rotatedEndPoint);
+        Point rotatedStartPoint2 = new Point(TaflConfig.BOARD_SIZE - 1 - rotatedStartPoint.y, rotatedStartPoint.x);
+        Point rotatedEndPoint2 = new Point(TaflConfig.BOARD_SIZE - 1 - rotatedEndPoint.y, rotatedEndPoint.x);
+        int rotatedAction2 = TaflUtils.getActionNumberFromMove(rotatedStartPoint2, rotatedEndPoint2);
+        Point rotatedStartPoint3 = new Point(TaflConfig.BOARD_SIZE - 1 - rotatedStartPoint2.y, rotatedStartPoint2.x);
+        Point rotatedEndPoint3 = new Point(TaflConfig.BOARD_SIZE - 1 - rotatedEndPoint2.y, rotatedEndPoint2.x);
+        int rotatedAction3 = TaflUtils.getActionNumberFromMove(rotatedStartPoint3, rotatedEndPoint3);
+
+        symmetries[0] = actionKey;
+        symmetries[1] = verticalMirroredAction;
+        symmetries[2] = horizontalMirroredAction;
+        symmetries[3] = rotatedAction;
+        symmetries[4] = rotatedAction2;
+        symmetries[5] = rotatedAction3;
+        return symmetries;
     }
 
     @Override
@@ -169,6 +193,47 @@ public class XNTupleFuncsTafl
             adjacencySet.add(TaflUtils.pointToCell(neighbor));
         }
         return adjacencySet;
+    }
+
+    private enum Rotation
+    {
+        CLOCKWISE, COUNTERCLOCKWISE
+    }
+
+    /**
+     * Rotates a board vector by a given rotation
+     * @param boardVector The vector representing the game board
+     * @param rotation The rotation direction
+     * @return the resulting board vector
+     */
+    private BoardVector rotateBoard(BoardVector boardVector, Rotation rotation)
+    {
+        int[] rotatedVector = new int[TaflConfig.TILE_COUNT];
+
+        if (rotation == Rotation.CLOCKWISE)
+        {
+            for (int i = 0; i < TaflConfig.BOARD_SIZE; i++)
+            {
+                for (int j = TaflConfig.BOARD_SIZE - 1; j >= 0; j--)
+                {
+                    int targetPos = i * TaflConfig.BOARD_SIZE + Math.abs(j - (TaflConfig.BOARD_SIZE - 1));
+                    int pos = j * TaflConfig.BOARD_SIZE + i;
+                    rotatedVector[targetPos] = boardVector.bvec[pos];
+                }
+            }
+        }
+        else
+        {
+            for (int i = TaflConfig.BOARD_SIZE - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < TaflConfig.BOARD_SIZE; j++)
+                {
+                    int targetPos = Math.abs(i - (TaflConfig.BOARD_SIZE - 1)) * TaflConfig.BOARD_SIZE + j;
+                    int pos = j * TaflConfig.BOARD_SIZE + i;
+                    rotatedVector[targetPos] = boardVector.bvec[pos];
+                }
+            }
+        }
     }
 
     private enum Axis
